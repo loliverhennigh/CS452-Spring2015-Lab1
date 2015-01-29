aabd429fb7bfc80a992abc68004a0ab7d4cfa4c7
csguest <csguest@itl.cslabs.clarkson.edu>
2015-01-29 15:50:53 -0500

    haow

--- lab1.js
+++ lab1.js
@@ -10,11 +10,18 @@ window.onload = function init()
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
 
+    var vertices = [
+	    vec2(-1, -1),
+	    vec2(0, 1),
+	    vec2(1, 0),
+	    vec2( 1, -1)
+    ];
+
     //
     //  Configure WebGL
     //
     gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+    gl.clearColor( 0.0, 0.0, 1.0, 1.0 );
     
     //  Load shaders and initialize attribute buffers
     
@@ -23,7 +30,16 @@ window.onload = function init()
     
     // Load the data into the GPU
 
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
     // Associate our shader variables with our data buffer
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
 
     render();
 };
@@ -31,4 +47,5 @@ window.onload = function init()
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays( gl.TRIANGLE_FAN, 0, 4);
 }
